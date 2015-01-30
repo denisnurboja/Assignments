549781a4cf3b68932139202b3c888b5cc54c3aae
Tony Gaeta <tony.gaeta@gmail.com>
2015-01-30 11:03:00 -0500

    completed assignment 13

---
+++ assignment13.rb
@@ -0,0 +1,22 @@
+# Create a guessing game
+# The user must guess the number chosen by the app
+# Provide feedback to the user as to if their guess
+#   is lower or higher than the number
+# Count the number of guesses and output at the end
+
+answer = rand(1..100)
+count = 0
+puts 'Guess a number randomly choosen this app, ranging from 1 to 100.'
+# Using a loop do conditional to reprompt user to input until guess == answer
+loop do
+  guess = gets.chomp.to_i
+  count += 1
+  if guess > answer
+    puts 'Your guess is too high. Guess lower please.'
+  elsif guess < answer
+    puts 'Your guess is too low. Guess higher please.'
+  else
+    puts "Finally! It only took you #{count} guesses."
+    exit
+  end
+end
