c04eb1e053d75e6614fecb1fda884642e7630768
Tony Gaeta <tony.gaeta@gmail.com>
2015-01-31 01:28:37 -0500

    commiting assignment 12 for review

---
+++ assignment12.rb
@@ -0,0 +1,13 @@
+# Assignment 12
+answers = []
+questions = ['How old are you?', 'What is your name?', 'Where are you from?']
+questions.each do |question|
+  puts "#{question}"
+  response = gets.chomp
+  answers.push("#{response}")
+end
+summary = []
+puts 'Summary of our very quick Q&A:'
+puts questions[0] + ' = ' + answers[0]
+puts questions[1] + ' = ' + answers[1].capitalize
+puts questions[2] + ' = ' + answers[2].capitalize
