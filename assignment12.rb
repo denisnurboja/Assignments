e3cd771e8b45041aaf8ddbed8cf9d2c9286a6a24
Tony Gaeta <tony.gaeta@gmail.com>
2015-01-31 11:04:51 -0500

    removed interpolation from question and response

--- assignment12.rb
+++ assignment12.rb
@@ -2,9 +2,9 @@
 answers = []
 questions = ['How old are you?', 'What is your name?', 'Where are you from?']
 questions.each do |question|
-  puts "#{question}"
+  puts question
   response = gets.chomp
-  answers.push("#{response}")
+  answers.push(response)
 end
 puts 'Summary of our very quick Q&A:'
 puts questions[0] + ' = ' + answers[0]
