bf79cd84c2ce32811eacd82256e61d03735ae364
Tony Gaeta <tony.gaeta@gmail.com>
2015-01-31 21:00:31 -0500

    re-did bonus: split text, .each, do loop

--- assignment14.rb
+++ assignment14.rb
@@ -1,35 +1,31 @@
-# Note for this exercise, follow these simplified pig latin rules
 # If the first letter is a vowel, add "way" to the end
 # If the first letter is a consonant, move it to the end and add "ay"
 
 class PigLatin
   VOWELS =  %w(a e i o u)
 
-  def self.pigatize(passed_text)
-
-    # Check to see if the first letter is a vowel, if not it's a consonant
-    if PigLatin.starts_with_vowel(passed_text[0])
-      puts "#{passed_text}" + "way"
-      pigatized_text = passed_text
-    else
-      puts "#{passed_text}"[1..-1] + "#{passed_text}"[0] + "ay"
-      pigatized_text = passed_text
+  def self.pigatize(text)
+    # Declare the output string
+    piglatin = ''
+    # Split the input text into words and loop with .each,
+    #   and 'word' as the iterator variable
+    text.split(' ').each do |word|
+      # Check to see if the first letter is a vowel, else it's a consonant
+      if PigLatin.vowel(word[0])
+        # Add onto the output string with += and add an extra space
+        piglatin += word + 'way' + ' '
+      else
+        piglatin += word[1..-1] + word[0] + 'ay' + ' '
+      end
     end
-    return pigatized_text
+    # Added a .chomp here to get rid of any trailing space
+    piglatin.chomp
   end
-
-  def self.pigatize_phrase(passed_text)
-    "#{passed_text}".split.each do |n|
-      "#{PigLatin.pigatize(n)}" + "way"
-    end
-  end
-
   # Check to see if the first letter is a vowel
-  def self.starts_with_vowel(first_letter)
+  def self.vowel(first_letter)
     VOWELS.include?(first_letter)
   end
 end
-
-puts "Please enter a word or a phrase and I will translate to pig latin"
-text = gets.chomp.to_s
-puts "Pigatized: #{PigLatin.pigatize_phrase(text).join(" ")}"
+puts 'Please enter a word and I will translate it into Pig Latin. Ippyyay!.'
+text = gets.chomp
+puts 'Pigatized: ' + PigLatin.pigatize(text)
