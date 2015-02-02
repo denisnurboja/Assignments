9594af438e743258592567b88bf2fb10ab5f5db6
Tony Gaeta <tony.gaeta@gmail.com>
2015-02-02 10:05:24 -0500

    renamed methods back to what the sr. gave me

--- assignment14.rb
+++ assignment14.rb
@@ -4,14 +4,14 @@
 class PigLatin
   VOWELS =  %w(a e i o u)
 
-  def self.pigatize(text)
+  def self.pigatize(passed_text)
     # Declare the output string
     piglatin = ''
     # Split the input text into words and loop with .each,
     #   and 'word' as the iterator variable
-    text.split(' ').each do |word|
+    passed_text.split(' ').each do |word|
       # Check to see if the first letter is a vowel, else it's a consonant
-      if PigLatin.vowel(word[0])
+      if PigLatin.starts_with_vowel(word[0])
         # Add onto the output string with += and add an extra space
         piglatin += word + 'way' + ' '
       else
@@ -22,10 +22,11 @@ class PigLatin
     piglatin.chomp
   end
   # Check to see if the first letter is a vowel
-  def self.vowel(first_letter)
-    VOWELS.include?(first_letter)
+  def self.starts_with_vowel(first_letter)
+    # this should determine if it starts with a vowel
+    first_letter
   end
 end
-puts 'Please enter a word and I will translate it into Pig Latin. Ippyyay!.'
+puts 'Please enter a word and I will translate it into Pig Latin. Ippyyay!'
 text = gets.chomp
 puts 'Pigatized: ' + PigLatin.pigatize(text)
