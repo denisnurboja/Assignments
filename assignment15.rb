a9ca4e2c3b5f85fa90180737077b25442abca052
Tony Gaeta <tony.gaeta@gmail.com>
2015-02-02 20:39:03 -0500

    changed bottom to mirror assignment 12; was opening the file twice: fixed

--- assignment15.rb
+++ assignment15.rb
@@ -1,17 +1,17 @@
 # Assignment 15
 
-# Grab questions from assignment15.txt to replace array
-# This should take only 2 lines of code to do
-questions = open('assignment15.txt', 'a+') do |q|
-  File.read('assignment15.txt').split(/\r?\n/)
-end
+questions = File.readlines('assignment15.txt').each(&:chomp!)
 answers = []
 questions.each do |question|
   puts question
   response = gets.chomp
   answers.push(response)
 end
+
 puts 'Summary of our very quick Q&A:'
-puts questions[0] + ' = ' + answers[0]
-puts questions[1] + ' = ' + answers[1].capitalize
-puts questions[2] + ' = ' + answers[2].capitalize
+summary = Hash[*questions.zip(answers).flatten]
+
+summary.each do |thequestions, theanswers|
+  puts "The question was: #{thequestions}"
+  puts "- Your answer was: #{theanswers.capitalize}"
+end
