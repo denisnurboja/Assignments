b0c1925b48ac037e2e19f5e78b8ce670e9381406
Tony Gaeta <tony.gaeta@gmail.com>
2015-02-04 21:52:47 -0500

    removed redundant parser from line 13; cleaned up the sentences

--- assignment20.rb
+++ assignment20.rb
@@ -9,17 +9,16 @@ d = Date.current
 print 'The date of the current week is: ', d.beginning_of_week, "\n"
 
 print 'The date in 30 days will be: ', d + 30.days, "\n"
-one_yr_from_td = d + 1.year
-solution = Date.parse(one_yr_from_td.to_s)
 
-print 'The day of the week in one year (from today) will be: ', \
-      solution.strftime('%A'), "\n"
+one_yr_from_td = d + 1.year
+print 'The day of the week in one year (from today) will be a: ', \
+      one_yr_from_td.strftime('%A'), "\n"
 
 print 'When is your birthday? (Format: MM/DD or ' \
       'MM-DD)', "\n"
 birthday = gets.chomp
 bd = Date.parse(2015.to_s + '/' + birthday)
-print 'Your birthday falls on ', bd.strftime('%A'), ' this year!', "\n"
+print 'Your birthday falls on a ', bd.strftime('%A'), ' this year!', "\n"
 
 print 'Enter a plural word & receive the singular version of the word.', "\n"
 word = gets.chomp.capitalize
