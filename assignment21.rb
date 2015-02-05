5a507c4ec832506625c91959bb589f7efad48779
Tony Gaeta <tony.gaeta@gmail.com>
2015-02-05 00:09:19 -0500

    made all corrections to errors; submitting for final review

--- assignment21.rb
+++ assignment21.rb
@@ -1,8 +1,8 @@
 # Assignment 21
 
 class Person
-  attr_reader :first_name, :last_name, :age
-  attr_accessor :birthday
+  attr_reader :first_name, :last_name
+  attr_accessor :birthdate
   require 'Date'
   require 'active_support'
   require 'active_support/all'
@@ -12,30 +12,21 @@ class Person
     @@person_count += 1
   end
 
-  def name(input)
+  def name=(input)
     person_name = input.split
     @first_name = person_name[0]
     @last_name = person_name[1]
   end
 
-  def bd(bdinput)
-    @birthday = Date.parse(bdinput)
-  end
-
-  def findage(birthday)
-    calculate_age(birthday)
+  def age
+    calculateAge
   end
 
   private
 
-  def calculate_age(birthday)
-    bd = Date.parse(birthday)
-    today = Date.current
-    age = today.strftime('%Y').to_i - bd.strftime('%Y').to_i
-    bd_this_yr = today.strftime('%Y') + '-' + bd.strftime('%m') + \
-                 '-' + bd.strftime('%d')
-    age -= 1  if today < Date.parse(bd_this_yr)
-    @age = age
+  def calculateAge
+    dob = Time.now - Time.parse(@birthdate)
+    age = dob.to_i / 60 / 60 / 24 / 365
   end
 end
 
@@ -44,16 +35,16 @@ loop do
   puts 'Hello user. What is your name?'
   input = gets.chomp
   break if input == '\q'
-  p.name(input)
+  p.name = input
   print ' ', "\n", 'Nice to meet you ', p.first_name.capitalize, '.', "\n"
   print 'When were you born? *YYYY/MM/DD or YYYY-MM-DD*', "\n"
-  bdinput = gets.chomp
-  p.bd(bdinput)
-  p.findage(bdinput)
+  p.birthdate = gets.chomp
+  bday = Date.parse(p.birthdate)
+  bd = bday.strftime('%A, %B %d, %Y, ')
   print ' ', "\n", 'Name: First: ', p.first_name.capitalize, ' | Last: ', \
         p.last_name.capitalize, "\n"
-  print 'You were born on ', p.birthday.strftime('%A, %B %d, %Y, '), \
-        "which makes you #{p.age} years old today.", "\n"
+  print 'You were born on ', bd, "which makes you #{p.age} " \
+        'years old today.', "\n"
   print 'You are User#: ', Person.person_count, "\n", ' ', "\n"
   break if input == '\q'
 end
