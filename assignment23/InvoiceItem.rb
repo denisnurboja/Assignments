8c88822f1afd68113cecf15a944929c68fea0e43
Tony Gaeta <tony.gaeta@gmail.com>
2015-02-08 21:48:12 -0500

    didn't need to initialize

--- assignment23/InvoiceItem.rb
+++ assignment23/InvoiceItem.rb
@@ -2,11 +2,4 @@
 
 class InvoiceItem
   attr_accessor :product_name, :sale_price, :quantity, :tax_percentage
-
-  def initialize
-    @product_name   = product_name
-    @sale_price     = sale_price
-    @quantity       = quantity
-    @tax_percentage = tax_percentage
-  end
 end
