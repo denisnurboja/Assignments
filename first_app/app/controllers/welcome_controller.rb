dc9fbb4994a75059dcaae538f77ec68c6b73f238
Tony Gaeta <tony.gaeta@gmail.com>
2015-02-11 13:06:06 -0500

    Removed first app directory

--- first_app/app/controllers/welcome_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    @title = params['title']
-
-    # Code to connect and query Rotten Tomatoes.
-
-    @synopsis = 'Great movie!'
-  end
-end
