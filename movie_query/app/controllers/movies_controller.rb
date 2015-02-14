732354959ceef230ac602507e7eea65b45c0fe66
Tony Gaeta <tony.gaeta@gmail.com>
2015-02-13 21:25:56 -0500

    added bootstrap, font-awesome, type-kit, google fonts, favicon

--- movie_query/app/controllers/movies_controller.rb
+++ movie_query/app/controllers/movies_controller.rb
@@ -1,12 +1,28 @@
 class MoviesController < ApplicationController
   def search
     @title = params['title']
+
     query_url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?'
     api = 'apikey=za4jav7tmcasvd3r6qfeutat'
     uri = URI(query_url + api + "&q=#{@title}")
     raw_json = Net::HTTP.get(uri)
     parsed_data = JSON.parse(raw_json)['movies']
-    @title = parsed_data.first['title']
-    @synopsis = parsed_data.first['synopsis']
+    movie = parsed_data.first
+
+    @cast = []
+    @title = movie['title']
+    @synopsis = movie['synopsis']
+    @year = movie['year']
+    @runtime = movie['runtime']
+    @critics_r = movie['ratings']['critics_rating']
+    @audience_r = movie['ratings']['audience_rating']
+    @critics = movie['ratings']['critics_score']
+    @audience = movie['ratings']['audience_score']
+    @theater = Date.parse(movie['release_dates']['theater'])
+    @dvd = Date.parse(movie['release_dates']['dvd'])
+    @posters = movie['posters']['original']
+    @poster = @posters.gsub('_tmb.jpg', '_ori.jpg')
+    @cast = movie['abridged_cast']
+    @rating = movie['mpaa_rating']
   end
 end
