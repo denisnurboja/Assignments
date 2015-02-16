35987ca59f59d91243c979a0e56fd584daac2a8e
Tony Gaeta <tony.gaeta@gmail.com>
2015-02-16 18:43:52 -0500

    added search form, added index view and set as root

--- movie_query/app/controllers/movies_controller.rb
+++ movie_query/app/controllers/movies_controller.rb
@@ -10,19 +10,39 @@ class MoviesController < ApplicationController
     movie = parsed_data.first
 
     @cast = []
+
     @title = movie['title']
     @synopsis = movie['synopsis']
+    if @synopsis == nil
+      @synopsis = 'No synopsis available at this time.'
+    else
+      @synopsis
+    end
+
     @year = movie['year']
     @runtime = movie['runtime']
     @critics_r = movie['ratings']['critics_rating']
     @audience_r = movie['ratings']['audience_rating']
     @critics = movie['ratings']['critics_score']
     @audience = movie['ratings']['audience_score']
-    @theater = Date.parse(movie['release_dates']['theater'])
-    @dvd = Date.parse(movie['release_dates']['dvd'])
     @posters = movie['posters']['original']
     @poster = @posters.gsub('_tmb.jpg', '_ori.jpg')
     @cast = movie['abridged_cast']
     @rating = movie['mpaa_rating']
+
+    @theater = movie['release_dates']['theater']
+    @dvd = movie['release_dates']['dvd']
+    if @dvd == nil
+      @dvd = 'No release date available at this time.'
+    else
+      @dvd = Date.parse(movie['release_dates']['dvd'])
+      @dvd = @dvd.strftime('%B %-d, %Y')
+    end
+    if @theater == nil
+      @theater = 'No release date available at this time.'
+    else
+      @theater = Date.parse(movie['release_dates']['theater'])
+      @theater = @theater.strftime('%B %-d, %Y')
+    end
   end
 end
