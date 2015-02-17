dca00463e611009de14a64e30bfcf16ac1374083
Tony Gaeta <tony.gaeta@gmail.com>
2015-02-17 16:40:09 -0500

    added styling and landing page

--- supercomputers/config/routes.rb
+++ supercomputers/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :supercomputers
 
-  root 'supercomputers#index'
+  root 'supercomputers#landing'
+
+  get 'index' => 'supercomputers#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
