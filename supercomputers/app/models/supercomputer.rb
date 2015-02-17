de757de8f083f987850622f679679f2d901e34fe
Tony Gaeta <tony.gaeta@gmail.com>
2015-02-17 14:07:15 -0500

    added Annotate gem

--- supercomputers/app/models/supercomputer.rb
+++ supercomputers/app/models/supercomputer.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: supercomputers
+#
+#  id         :integer          not null, primary key
+#  rank       :integer
+#  name       :string
+#  city       :string
+#  country    :string
+#  speed      :float
+#  memory     :integer
+#  power      :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Supercomputer < ActiveRecord::Base
 end
